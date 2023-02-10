5d5159ca7c6844ba0923c1ace4b9aa9dc8e8178a
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-02-10 01:23:13 -0800

    updating

--- 361_Assignment/Assignment 2/Assignment2.m
+++ 361_Assignment/Assignment 2/Assignment2.m
@@ -1,7 +1,8 @@
-image1 = imread("2.jpg");
+clear;
+image1 = imread("2.png");
 image1 = rgb2gray(image1);
 image1 =im2double(image1);
-targetSize = [750 750];
+targetSize = [1500 1500];
 r = centerCropWindow2d(size(image1),targetSize);
 image1 = imcrop(image1,r);
 image1 =imresize(image1,[750,750]);
@@ -9,10 +10,10 @@ Fast1 = my_fast_detector(image1,0.05);
 Faster1 = Harris(image1,Fast1);
 
 
-image2 = imread("1.JPG");
+image2 = imread("1.png");
 image2 = rgb2gray(image2);
 image2 =im2double(image2);
-targetSize = [750 750];
+targetSize = [1500 1500];
 r = centerCropWindow2d(size(image2),targetSize);
 image2 = imcrop(image2,r);
 image2 =imresize(image2,[750,750]);
@@ -39,8 +40,43 @@ matchedPoints2 = validPoints2(indexPairs(:, 2), :);
 figure;
 showMatchedFeatures(image1, image2, matchedPoints1, matchedPoints2,'montage');
 
-[tform,inlierIdx] = estgeotform2d(matchedPoints1, matchedPoints2,'projective', 'confidence', 99.9, 'MaxNumTrials', 2000);
 
+
+
+imageSize = [targetSize;targetSize];
+tforms2 = estgeotform2d(matchedPoints1, matchedPoints2,'projective', 'confidence', 99, 'MaxNumTrials', 2000);
+tforms2 = invert(tforms2);
+tforms1 = projtform2d;
+
+
+[xlim(1,:), ylim(1,:)] = outputLimits(tforms1, [1 imageSize(1,2)], [1 imageSize(1,1)]);
+[xlim(2,:), ylim(2,:)] = outputLimits(tforms2, [1 imageSize(2,2)], [1 imageSize(2,1)]);
+maxImageSize = max(imageSize);
+xMin = min([1; xlim(:)]);
+xMax = max([maxImageSize(2); xlim(:)]);
+yMin = min([1; ylim(:)]);
+yMax = max([maxImageSize(1); ylim(:)]);
+width  = round(xMax - xMin);
+height = round(yMax - yMin);
+panorama = zeros(height,width);
+%panorama = zeros(2000,2000);
+
+
+blender = vision.AlphaBlender('Operation', 'Binary mask', 'MaskSource', 'Input port');  
+xLimits = [xMin xMax];
+yLimits = [yMin yMax];
+panoramaView = imref2d([height width], xLimits, yLimits);
+
+warpedImage1 = imwarp(image1, tforms1, 'OutputView', panoramaView);
+mask1 = imwarp(true(size(image1,1),size(image1,2)), tforms1, 'OutputView', panoramaView);
+panorama = step(blender, panorama, warpedImage1, mask1);
+
+warpedImage2 = imwarp(image2, tforms2, 'OutputView', panoramaView);
+mask2 = imwarp(true(size(image2,1),size(image2,2)), tforms2, 'OutputView', panoramaView);
+panorama = step(blender, panorama, warpedImage2, mask2);
+
+figure
+imshow(panorama);
 
 
 
