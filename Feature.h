78f2235e09d5c88b485d3e4dccf1dc98497cbf81
David_Qian_0504 <94476615+RSQ0504@users.noreply.github.com>
2023-02-09 18:04:28 -0800

    add the function signal

--- Feature.h
+++ Feature.h
@@ -11,7 +11,13 @@ using namespace std;
  * @param keypoints 
  */
 void createSIFT(Mat image,vector<KeyPoint> &keypoints);
-
+/**
+ * @brief 
+ * 
+ * @param image 
+ * @param keypoints 
+ * @param threshold 
+ */
 void createSIFT(Mat image,vector<KeyPoint> &keypoints,double threshold);
 /**
  * @brief 
@@ -25,9 +31,26 @@ void createSIFT(Mat image,vector<KeyPoint> &keypoints,double threshold);
 void matchSiftFeatures(Mat image1, Mat image2,
                         vector<KeyPoint> &keypoints1,vector<KeyPoint> &keypoints2,
                         vector<DMatch> &matches);
-
+/**
+ * @brief 
+ * 
+ * @param image1 
+ * @param image2 
+ * @param keypoints1 
+ * @param keypoints2 
+ * @param matches 
+ * @param threshold 
+ */
 void matchSiftFeatures(Mat image1, Mat image2,
                         vector<KeyPoint> &keypoints1,vector<KeyPoint> &keypoints2,
                         vector<DMatch> &matches,double threshold);
-
+/**
+ * @brief 
+ * 
+ * @param image 
+ * @param out 
+ * @param size 
+ * @param threshold 
+ * @return Mat 
+ */
 Mat HarrisCornerDetector(Mat image,Mat &out,int size, int threshold);
